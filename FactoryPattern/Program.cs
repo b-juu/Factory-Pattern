cfbda54185807e4554478ab8795ce156ad617914
b-juu <erichassett4@gmail.com>
2023-10-16 11:17:02 -0400

    init commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number of tires on the vehicle: ");
+            if (int.TryParse(Console.ReadLine(), out int tireCount))
+            {
+                try
+                {
+                    IVehicle vehicle = VehicleFactory.GetVehicle(tireCount);
+                    vehicle.Drive();
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number of tires.");
+            }
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int tireCount)
+        {
+            switch (tireCount)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4:
+                    return new Car();
+                case 18:
+                    return new BigRig();
+                default:
+                    throw new ArgumentException("Invalid number of tires. Cannot create a vehicle.");
+            }
+        }
+    }
+}
